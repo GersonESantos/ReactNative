af549a1c9ec0c2b71f3bddc46fd1e9ce28f7d3a6
GersonESS <gebhsantos@gmail.com>
2025-03-17 16:35:01 -0300

    a

---
+++ yt/yt02/MyApp/src/app/index.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from "react-native";
+export default function Index() {
+    return (
+        <View>
+            <Text style={{ color: "red", fontSize: 24}}>Ola Yt00</Text>
+        </View>
+    );
+}
