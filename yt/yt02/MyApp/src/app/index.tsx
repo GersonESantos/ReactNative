71738962e3e3fb5b5b55babe8476e560d727a9e7
GersonESS <gebhsantos@gmail.com>
2025-03-17 16:37:58 -0300

    Update index.tsx

--- yt/yt02/MyApp/src/app/index.tsx
+++ yt/yt02/MyApp/src/app/index.tsx
@@ -2,7 +2,7 @@ import { View, Text } from "react-native";
 export default function Index() {
     return (
         <View>
-            <Text style={{ color: "red", fontSize: 24}}>Ola Yt00</Text>
+            <Text style={{ color: "red", fontSize: 24}}>Ola Yt02</Text>
         </View>
     );
 }
