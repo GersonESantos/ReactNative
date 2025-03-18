3a54405f18a1f57e2a9fd5f6b387b6631f884c72
GersonESS <gebhsantos@gmail.com>
2025-03-18 03:40:27 -0300

    a

---
+++ zero/03zero/MeuApp/App.js
@@ -0,0 +1,11 @@
+import React from  "react";
+import { Text, View } from  "react-native";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent:  "center", alignItems:  "center" }}>
+      <Text style={{ fontSize: 25, fontWeight: '800', color: '#F00'}}>Olá GersonES</Text>
+    </View>
+  );
+}
+
