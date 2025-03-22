6a400f4af5949d0a25aac276dc761663ddf51119
GersonESS <gebhsantos@gmail.com>
2025-03-22 13:54:18 -0300

    a

--- yt/yt09/MyApp/src/app/index.tsx
+++ yt/yt09/MyApp/src/app/index.tsx
@@ -1,22 +1,23 @@
-import { View, Text, StyleSheet, Alert } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, } from "react-native";
+import { router } from "expo-router";
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
-import { useState } from "react";
+
 
 export default function Index() {
 
     const [name, setName] = useState("")
-    function handleMessage() {
-        const name = "GersonES"
-        Alert.alert("Hello " + name + "!");
+    function handleNext() {
+        router.navigate("./dashbord");
     } 
     return (
         <View style={styles.container}>
         <Text style={styles.title}>Ola, {name}</Text>
         <Input onChangeText={(text => setName(text))} placeholder="Digite seu nome" />      
-        <Button title="Entrar" onPress={handleMessage}/>
-        <Button title="Continuar" onPress={handleMessage}/>
-        <Button title="Sair" onPress={handleMessage}/>
+        <Button title="Entrar" onPress={handleNext}/>
+        <Button title="Continuar" onPress={handleNext}/>
+        <Button title="Sair" onPress={handleNext}/>
         </View>
     );
     }
