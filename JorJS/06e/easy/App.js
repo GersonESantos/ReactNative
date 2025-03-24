e64e1660c591547668a89da2dee43b691052b47e
GersonESS <gebhsantos@gmail.com>
2025-03-24 17:26:18 -0300

    a

---
+++ JorJS/06e/easy/App.js
@@ -0,0 +1,11 @@
+import Home from './src/screens/home/home.jsx';
+
+import Passenger from './src/screens/passenger/passenger.jsx';
+
+export default function App() {
+  return (
+    
+    <Passenger />
+      
+  );
+}
