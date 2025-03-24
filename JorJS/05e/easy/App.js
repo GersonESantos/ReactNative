00a26f4e741fbcc8f3f2bb2f8ce816a18225dbf8
GersonESS <gebhsantos@gmail.com>
2025-03-24 17:06:56 -0300

    d

--- JorJS/05e/easy/App.js
+++ JorJS/05e/easy/App.js
@@ -1,10 +1,11 @@
 import Home from './src/screens/home/home.jsx';
-import {  Text } from 'react-native';
+
+import Passenger from './src/screens/passenger/passenger.jsx';
 
 export default function App() {
   return (
     
-    <Home />
+    <Passenger />
       
   );
 }
--- JorJS/05e/easy/src/screens/home/home.jsx
+++ JorJS/05e/easy/src/screens/home/home.jsx
@@ -1,9 +1,11 @@
 import { Text } from 'react-native';
 function Home() {
   return (
-    <Text style={{fontSize: 50,
-                  color: 'blue' 
-    }} >Home</Text>
+    <>
+    <Text style={{fontSize: 50, color: 'blue' }} >Logotipo</Text>
+    <Text style={{fontSize: 50, color: 'blue' }} >Passageiros</Text>
+    <Text style={{fontSize: 50, color: 'blue' }} >Motorista</Text>
+    </>
   );
 }
 export default Home;
