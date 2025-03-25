2e5a980389ad54a629b67dd39c8b4347c7584e1a
GersonESS <gebhsantos@gmail.com>
2025-03-25 09:15:58 -0300

    a

---
+++ JorJS/09e/easy/src/screens/passenger/passenger.jsx
@@ -0,0 +1,11 @@
+import { Text } from 'react-native';
+function Passenger() {
+  return ( 
+    <>
+    <Text>Passenger</Text>
+    <Text>Mapa</Text>
+    <Text>Origem Destino</Text>    
+    </>
+  );   
+} 
+export default Passenger;
