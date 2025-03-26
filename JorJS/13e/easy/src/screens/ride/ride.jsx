d0e51e15fa7bca4ea3f9d43462d89c757c88b4ff
GersonESS <gebhsantos@gmail.com>
2025-03-26 07:31:59 -0300

    a

---
+++ JorJS/13e/easy/src/screens/ride/ride.jsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native';
+import { styles } from './ride.style.js';
+function Ride(props) {
+  return ( 
+    <>
+    <Text>Ride</Text>
+    <Text>Lista</Text>
+    
+    </>
+  );   
+} 
+export default Ride;
---
+++ JorJS/13e/easy/src/screens/ride/ride.style.js
@@ -0,0 +1,29 @@
+export const styles = {
+    bg: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    logo: {
+      width: 200,
+      height: 46,
+    },
+    img: {
+      width: 160,
+      height: 160
+    },
+    title: {
+      color: '#fff',
+      fontSize: 22,
+      fontWeight: 'bold'
+    },
+    text: {
+      color: '#fff',
+      fontSize: 14
+    },
+    btn: {
+       alignItems: 'center',
+       marginTop: 20
+    }
+};
+export default styles;
