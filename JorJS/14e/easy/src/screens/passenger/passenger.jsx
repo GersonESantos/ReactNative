9d0caf6badfeb5aea99323245ff89c938e7561f3
GersonESS <gebhsantos@gmail.com>
2025-03-27 00:25:03 -0300

    Update passenger.jsx

--- JorJS/14e/easy/src/screens/passenger/passenger.jsx
+++ JorJS/14e/easy/src/screens/passenger/passenger.jsx
@@ -1,12 +1,14 @@
 import { Text, View } from 'react-native';
 import  MyButton  from '../../components/myButton/myButton.jsx';
-import MapView from 'react-native-maps';
+import MapView, { PROVIDER_DEFAULT }  from 'react-native-maps';
 import { styles } from './passenger.syte.js';
 function Passenger(props) {
   return  ( 
     <View style={styles.container}>
      
-      <MapView style={styles.map}/>
+      <MapView style={styles.map} 
+      provider={PROVIDER_DEFAULT} >
+      </MapView>
       <MyButton text="TESTE"/>   
     </View>
   );
