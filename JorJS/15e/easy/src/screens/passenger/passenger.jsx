06ad862a812b9501e50c1b8503531d84de8046cf
GersonESS <gebhsantos@gmail.com>
2025-03-29 17:49:15 -0300

    a

--- JorJS/15e/easy/src/screens/passenger/passenger.jsx
+++ JorJS/15e/easy/src/screens/passenger/passenger.jsx
@@ -1,13 +1,28 @@
 import { Text, View } from 'react-native';
 import  MyButton  from '../../components/myButton/myButton.jsx';
-import MapView, { PROVIDER_DEFAULT }  from 'react-native-maps';
-import { styles } from './passenger.syte.js';
+import MapView, { Marker, PROVIDER_DEFAULT }  from 'react-native-maps';
+import { styles } from './passenger.sytle.js';
+import { useState } from 'react';
 function Passenger(props) {
+
+  const [myLocation, setMyLocation] = useState({
+    latitude: 20,
+    longitude: 20});
+
   return  ( 
     <View style={styles.container}>
-     
       <MapView style={styles.map} 
-      provider={PROVIDER_DEFAULT} >
+        provider={PROVIDER_DEFAULT} 
+      initialRegion={{
+                latitude: -19.910991,
+                longitude: -43.949690,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004
+            }}>
+      <Marker coordinate={{
+        latitude: -19.910991,
+        longitude: -43.949690
+      }}/>
       </MapView>
       <MyButton text="TESTE"/>   
     </View>
