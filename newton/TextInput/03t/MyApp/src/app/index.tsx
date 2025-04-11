a26fe398a3b04b73e668c793f44d259e9af6b3f0
GersonESS <gebhsantos@gmail.com>
2025-04-11 20:44:14 -0300

    a

---
+++ newton/TextInput/03t/MyApp/src/app/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { TextInput, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <TextInput placeholder="Digite algo..." 
+      style={styles.input}
+      keyboardType="email-address"
+      autoCapitalize="none"
+      
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'red',
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontSize: 16,
+  },
+});
