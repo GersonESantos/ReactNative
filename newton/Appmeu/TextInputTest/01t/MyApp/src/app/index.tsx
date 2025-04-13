4eea4df4d4c262dacaf11fb91f8b6c095ae739bc
GersonESS <gebhsantos@gmail.com>
2025-04-13 15:51:43 -0300

    Update index.tsx

--- newton/Appmeu/TextInputTest/01t/MyApp/src/app/index.tsx
+++ newton/Appmeu/TextInputTest/01t/MyApp/src/app/index.tsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import { TextInput, View } from 'react-native';
+import { View, Text, TextInput,StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
 
-export default function App() {
+
+
+export default function TextInputTest() {
+  const [nome, setNome] = useState("");
   return (
-    <View>
-      <TextInput placeholder="Digite algo..." />
-    </View>
+    <View style={style.container}>
+      <TextInput
+        placeholder="Nome"
+        value={nome}
+        onChangeText={setNome}
+      />    
+  </View>
   );
 }
+
+
+
+const style = StyleSheet.create({
+  container: { 
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "white",
+      gap: 40,    
+      },
+  title :{ 
+      color: "red", 
+      fontSize: 24,
+      fontWeight: "bold",
+  },
+  actions: {
+      padding: 24,
+      backgroundColor: "#14448080",
+      width: "80%",
+      borderRadius: 32,
+      borderWidth: 2,
+      borderColor: "#144480",
+      alignItems: "center",
+      gap: 40,
+  },
+  timer: {
+      fontSize: 54,
+      color: "#FFF",
+      fontWeight: "bold",
+      textAlign: "center"
+  },
+  
+button: {
+    padding: 24,
+      backgroundColor: "#B872ff",
+      borderRadius: 32,
+      marginTop: 12,
+  },
+  textButton: {
+    alignItems: "center",
+      color: "#FFF",    
+      fontSize: 16,
+      fontWeight: "bold",
+      textAlign: "center",  
+  },
+  footer: {
+      marginTop: 24,
+      alignItems: "center",
+  },
+  textfooter: {
+      fontSize: 18,
+      color: "#333",
+  },
+  });
