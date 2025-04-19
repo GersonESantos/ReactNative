2b6c76702eb9e9dbc01da0aec2abbf0738bb88b7
GersonESS <gebhsantos@gmail.com>
2025-04-18 22:15:23 -0300

    a

--- newton/05newApp/MyApp/src/app/index.tsx
+++ newton/05newApp/MyApp/src/app/index.tsx
@@ -1,28 +1,46 @@
-import { View, Text, StyleSheet, Alert, Button } from "react-native";
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
-    function handleMessage() {
-        const name = "GersonES"
-        Alert.alert("Hello " + name + "!");
-    }
-    return (
-        <View style={styles.container}>
-        <Text style={styles.title}>Ola GersonES!</Text>
-        <Button title="Enviar" onPress={handleMessage} />
-        </View>
-    );
-    }
+  const router = useRouter();
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={styles.container}>
+         <Pressable style={styles.botao} onPress={() => router.push('/telaA')}>
+          <Text style={styles.texto}>Ir para Tela A</Text>
+        </Pressable>
+        {/*<Pressable style={styles.botao} onPress={() => router.push('/telaB')}>
+          <Text style={styles.texto}>Ir para Tela B</Text>
+        </Pressable>
+        <Pressable style={styles.botao} onPress={() => router.push('/telaC')}>
+          <Text style={styles.texto}>Ir para Tela C</Text>
+        </Pressable>
+        <Pressable style={styles.botao} onPress={() => router.push('/TextInputTest')}>
+          <Text style={styles.texto}>TextInput</Text>
+        </Pressable> */
+        <Text>edit app/index</Text>}
+      </View>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    container: { 
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-         padding: 32    
-        },
-    title :{ 
-        color: "red", 
-        fontSize: 24,
-        fontWeight: "bold",
-    },
-    });
+  container: {
+    flex: 1,
+    backgroundColor: '',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 20
+  },
+  botao: {
+    backgroundColor: '#1E90FF',
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10
+  },
+  texto: {
+    color: '#fff',
+    fontSize: 18
+  }
+});
