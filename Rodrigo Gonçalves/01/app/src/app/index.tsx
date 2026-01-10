c7a3fe677238aa79b4fee2a68fe54235fc002e63
GersonESS <gebhsantos@gmail.com>
2026-01-09 21:37:32 -0300

    a

--- Rodrigo Gonçalves/01/app/src/app/index.tsx
+++ Rodrigo Gonçalves/01/app/src/app/index.tsx
@@ -1,10 +1,22 @@
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 export default function Index() {
   return (
-    <View>
-      <Text>Hello, World!</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, World!</Text>
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  title: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+});
 //# sourceMappingURL=index.js.map
