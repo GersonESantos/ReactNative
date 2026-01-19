02d689ee56de47984d6a58b0de161a9dda5f8532
GersonESS <gebhsantos@gmail.com>
2026-01-18 21:06:36 -0300

    a

---
+++ Rodrigo Gonçalves/rn/rnform/00/src/components/input/style.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextInput, View } from "react-native";
+import { Feather }   from '@expo/vector-icons';
+
+export function Input() {
+    return (
+        <View>
+            <TextInput />
+        </View> 
+    )
+}
+
--- Rodrigo Gonçalves/rn/rnform/00/src/screens/FormStepOne/index.tsx
+++ Rodrigo Gonçalves/rn/rnform/00/src/screens/FormStepOne/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from "react-native";
 import { styles } from "./styles";
+import { Input } from "../../components/input";
 export function FormStepOne() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
         FormStepOne 
       </Text>
+      <Input />
     </View>
   );
 }
